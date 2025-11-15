d609c1b574c5bf9ebafdbf2c79081a3955367f2e
unknown <josmarsanteliz@gmail.com>
2025-11-15 00:00:50 -0400

    dos proyectos más

---
+++ 06-tabs/vite.config.js
@@ -0,0 +1,21 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      
+      '/api': {
+        target: 'https://course-api.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), // Esto elimina /api del path
+      },
+    },
+  },
+})
+
+
+ 
